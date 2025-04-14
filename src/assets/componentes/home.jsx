fcf7a1cc18c4cb550edb4921e2712f48ba409b12
deeh275 <153244785+deeh275@users.noreply.github.com>
2025-04-14 20:55:05 -0300

    ajustes finais

--- src/assets/componentes/home.jsx
+++ src/assets/componentes/home.jsx
@@ -5,10 +5,13 @@ function Home() {
         <section id="home" className="home-container">
             <div className="home-content">
                 <h1 className="home-title">Transformo ideias em códigos e código em soluções eficientes.</h1>
+                <div className="text-container">
                 <TextoDigitando 
                     texto="Olá, meu nome é David, sou desenvolvedor Full Stack apaixonado por tecnologia e inovação. Minha missão é criar aplicações escaláveis, intuitivas e eficientes, sempre buscando a melhor experiência para o usuário."
                     velocidade={30}
                 />
+                </div>
+            
             </div>
         </section>
     );
