f620d90c9687815ad70a9a57c426921eaa2a8e7b
deeh275 <153244785+deeh275@users.noreply.github.com>
2025-07-07 20:17:14 -0300

    adição de novos projetos

--- src/assets/componentes/projetos.jsx
+++ src/assets/componentes/projetos.jsx
@@ -44,7 +44,7 @@ function Projetos() {
                         <p className="descricao-texto">App desenvolvido com React.js, CSS, Ionic e integração com uma biblioteca de filmes online (API externa), oferecendo uma interface moderna, responsiva e focada em dispositivos móveis.
 
                             Este aplicativo permite que os usuários realizem buscas interativas por filmes, filtrando por título. tornando a navegação prática e personalizada. O app também fornece recomendações personalizadas com base no gênero e ano de lançamento, promovendo uma experiência dinâmica e intuitiva.</p>
-                        <p></p>
+
                         <dvi>
                             <i
                                 className="devicon-react-original colored icon-projeto" title="React"
@@ -63,11 +63,36 @@ function Projetos() {
                 </div>
 
                 <div className="projeto-grid"> {/* Alterado para projeto-grid */}
-                    <div className="projeto-image"></div>
+                    <div className="projeto-image" style={{ backgroundImage: `url("skillhub.png")` }}></div>
                     <div className="projeto-descricao">
-                        <h2>Projeto 2</h2>
-                        <p>Descrição do projeto 2.</p>
+                        <h2 className="Title-descricao">Aplicativo de Serviços – Dryclenerc (em desenvolvimento)</h2>
+
+                        <p>Aplicativo desenvolvido para a empresa DryClenerc utilizando React.js, CSS e Vite, com backend próprio para gerenciar autenticação, dados e notificações. O projeto está em andamento e foca em uma interface responsiva e otimizada para dispositivos móveis.
+
+                            A plataforma possibilita buscas rápidas e filtradas para que clientes encontrem prestadores adequados, enquanto profissionais têm acesso a oportunidades reais de trabalho. Conta com cadastro de usuários, avaliações, feedbacks e sistema de notificações para acompanhamento dos pedidos.
+
+                            Integrações futuras com APIs externas visam ampliar os serviços disponíveis, proporcionando uma experiência dinâmica e personalizada para os usuários.</p>
+
+
+                        <dvi>
+                            <i
+                                className="devicon-react-original colored icon-projeto" title="React"
+                            ></i>
+                            <i
+                                className="devicon-css3-plain colored icon-projeto" title="CSS3"
+                            ></i>
+                            <i
+                                className="devicon-vite-original colored icon-projeto" title="vite"
+                            ></i>
+
+                            <i className="devicon-nodejs-plain colored icon-projeto" title="Node.js"></i>
+
+
+
+                        </dvi>
                     </div>
+
+
                 </div>
 
                 <div className="projeto-grid"> {/* Alterado para projeto-grid */}
